706448d50e4d6309d58c61a4d3cfcfe28dba4951
chaiths246 <chaiths246@gmail.com>
2017-07-11 21:04:34 -0700

    Removed unsed button from MainActivity

--- app/src/main/java/com/example/chaithra/quizapp/MainActivity.java
+++ app/src/main/java/com/example/chaithra/quizapp/MainActivity.java
@@ -3,13 +3,8 @@ package com.example.chaithra.quizapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button button;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
